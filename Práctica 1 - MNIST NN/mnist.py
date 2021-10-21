32ada33c789b47e3b32f70dda1c58f72dde17b35
LCinder <diegohm1598@gmail.com>
2021-10-21 19:47:57 +0200

    Funciona MNIST

--- Práctica 1 - MNIST NN/mnist.py
+++ Práctica 1 - MNIST NN/mnist.py
@@ -19,47 +19,53 @@ def digito(n):
     print("El numero es " + str(train_labels[n]))
 
 def inicializarWeights(train):
-    ws = []
-    #for i in range(10):
-    #    ws.append(numpy.zeros(numpy.shape(train)[1]*numpy.shape(train)[1]))
     return numpy.zeros(train.shape[1]*train.shape[1])
 
+def obtenerNumero(arr):
+    for i in range(10):
+        if arr[i] == 1:
+            return i
+    return -1
+
 def predecir(w, x):
-    res = numpy.inner(w, x.reshape(28*28))
-    return funcionActivacion(res)
+    res = funcionActivacion(numpy.dot(w, x.reshape(28*28)))
+    return res
 
 def funcionActivacion(pred):
     y_aux = []
-    #print(pred)
     for i in range(10):
-        if pred >= 0:
+        if pred[i] >= 0:
             y_aux.append(1)
         else:
            y_aux.append(0)
+
     return y_aux
 
 def perceptron(train, labels, epocas):
-    w = inicializarWeights(train)
-    ws = []
+    w_aux = []
+
+    for i in range(10):
+        w_aux.append(inicializarWeights(train))
 
     for i in range(epocas):
-        for x, y in zip(train, labels):
-            y_pred = predecir(w, x)
+        for x_i, y_i in zip(train, labels):
+            y_pred = predecir(w_aux, x_i)
             for j in range(10):
-                if y_pred[j] == 1 and y == j:
-                    w = w + numpy.reshape(x, 28*28)
-                else:
-                    w = w - numpy.reshape(x, 28*28)
-        ws.append(w)
+                if y_pred[j] == 1 and y_i != j:
+                    w_aux[j] = w_aux[j] - numpy.reshape(x_i, 28 * 28)
+                elif y_pred[j] == 0 and y_i == j:
+                    w_aux[j] = w_aux[j] + numpy.reshape(x_i, 28 * 28)
 
-    return ws
+    return w_aux
 
 if __name__=="__main__":
     train, train_labels, test, test_labels = loadDataset()
 
     #digito(0)
-    w = perceptron(train, train_labels, 10)
-    y = predecir(w, train[0])
-    res = funcionActivacion(y)
-    print(res)
+    n = 1
+    w = perceptron(train, train_labels, 1)
 
+    for i in range(50):
+        y = predecir(w, train[i])
+        y_n = obtenerNumero(y)
+        print("El nº es " + str(train_labels[i]) + " y el nº predicho es " + str(y_n))
