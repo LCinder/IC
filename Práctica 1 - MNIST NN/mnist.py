310b2e7cbf16789fdfa2b24b150b296897d90f71
diego <diegohm1598@gmail.com>
2021-11-30 12:24:15 +0100

    Arreglando errores

--- Práctica 1 - MNIST NN/mnist.py
+++ Práctica 1 - MNIST NN/mnist.py
@@ -3,6 +3,13 @@ import numpy
 import matplotlib.pyplot as plot
 from PIL import Image
 import matplotlib.pyplot as plot
+from keras import Input
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dropout, LSTM
+from keras.utils.np_utils import to_categorical
+from tensorflow import keras
+from keras.models import Sequential
+from tensorflow.keras.optimizers import SGD, Adam
+from keras.layers.core import Dense, Activation
 
 
 class Perceptron:
@@ -24,9 +31,9 @@ class Perceptron:
         return train, train_labels, test, test_labels
 
     def showDigit(self, n):
-        plot.imshow(train[n], cmap=plot.cm.binary)
+        plot.imshow(x_train[n], cmap=plot.cm.binary)
         plot.show()
-        print("El numero es " + str(train_labels[n]))
+        print("El numero es " + str(y_train[n]))
 
     def inicializarWeights(self, train):
         return numpy.random.rand(train.shape[1] * train.shape[1]) #numpy.zeros(train.shape[1] * train.shape[1])
@@ -44,7 +51,7 @@ class Perceptron:
 
     def saveImages(self, n, duration):
         self.images[0].save("gif/" + str(n) + ".gif", format="GIF", save_all=True, append_images=self.images,
-                            duration=duration, loop=0)
+        duration=duration, loop=0)
 
     def test(self, x_test, y_test):
         good = 0
@@ -104,25 +111,24 @@ class Perceptron:
         for l in range(len(self.w)):
             self.w[l] /= 255
 
-
         for i in range(epocas):
             accuracy_aux = 0
 
             for x_i, y_i, l in zip(x_train, y_train, range(len(x_train))):
                 y_pred = self.predict(self.w, x_i, "softmax") # aplicar sigmoide mejor
 
-                for j in range(1):
-                    if tipo == "softmax":
-                        z = numpy.dot(self.w, x_i.reshape(self.SIZE * self.SIZE))
-                        y_i_arr = []
-                        for k in range(self.DIGITS):
-                            if k == y_i:
-                                y_i_arr.append(1)
-                            else:
-                                y_i_arr.append(0) #derviada para propagar hacia atras y  funcion lineal para propagar hacia adelante
-                        delta_y = self.sigmoid(z) * (numpy.array(y_pred) - numpy.array(y_i_arr))
-                        self.w -= self.n * numpy.resize(numpy.outer(x_i.reshape(self.SIZE * self.SIZE),  numpy.resize(delta_y, (1, 10))), (10, 784))
-                    else:
+                if tipo == "softmax":
+                    z = numpy.dot(self.w, x_i.reshape(self.SIZE * self.SIZE))
+                    y_i_arr = []
+                    for k in range(self.DIGITS):
+                        if k == y_i:
+                            y_i_arr.append(1)
+                        else:
+                            y_i_arr.append(0) #derviada para propagar hacia atras y  funcion lineal para propagar hacia adelante
+                    delta_y = self.sigmoid(z) * (numpy.array(y_pred) - numpy.array(y_i_arr))
+                    self.w -= self.n * numpy.resize(numpy.outer(x_i.reshape(self.SIZE * self.SIZE),  numpy.resize(delta_y, (1, 10))), (10, 784))
+                else:
+                    for j in range(self.DIGITS):
                         if y_pred[j] == 1 and y_i != j:
                             self.w[j] = self.w[j] - numpy.reshape(x_i, self.SIZE * self.SIZE)
                         elif y_pred[j] == 0 and y_i == j:
@@ -142,20 +148,143 @@ class Perceptron:
         return self.w
 
 
+class MultiLayer:
+    def __init__(self, x, y, hidden, epochs, lr):
+        self.x = x
+        self.y = y
+        self.hidden = hidden
+        self.epochs = epochs
+        self.lr = lr
+        self.w = [numpy.random.rand(784) for i in range(10)]
+
+    def fit(self, x, y):
+        for i in range(self.epochs):
+            hidden_input = numpy.dot(x, self.w)
+            hidden_output = self.hidden_activation(hidden_input)
+            output_input = numpy.dot(hidden_output, numpy.random())
+            y_pred = self.output_activation(output_input)
+
+            grad = self.loss.gradient(y, y_pred) * self.output_activation.gradient(output_input)
+            grad_v = hidden_output.dot(grad)
+
+
+
+
+def NN(x_train, y_train, x_test, y_test, type):
+
+    if type == "cnn":
+        model = Sequential([
+            Input(shape=(28, 28, 1)),
+            # Red neuronal convolutiva con mascara 3x3
+            Conv2D(50, activation="relu", kernel_size=(3, 3)),
+            # Para evitar el sobreajuste se eliminan nodos aleatoriamente
+            Dropout(0.2),
+            MaxPooling2D(pool_size=(2, 2)),
+            Conv2D(100, activation="relu", kernel_size=(3, 3)),
+            # Para evitar el sobreajuste se eliminan nodos aleatoriamente
+            Dropout(0.2),
+            MaxPooling2D(pool_size=(2, 2)),
+            Conv2D(500, activation="relu", kernel_size=(3, 3)),
+            # Para evitar el sobreajuste se eliminan nodos aleatoriamente
+            Dropout(0.2),
+            MaxPooling2D(pool_size=(2, 2)),
+            Conv2D(1000, activation="relu", kernel_size=(3, 3)),
+            # Para evitar el sobreajuste se eliminan nodos aleatoriamente
+            Dropout(0.2),
+            MaxPooling2D(pool_size=(2, 2)),
+            Conv2D(10, activation="relu", kernel_size=(3, 3)),
+            # Para evitar el sobreajuste se eliminan nodos aleatoriamente
+            Dropout(0.2),
+            MaxPooling2D(pool_size=(2, 2)),
+            # Serializa(tranforma) un tensor(array)
+            Flatten(),
+            Dense(10, activation="softmax")
+        ])
+    elif type == "deep":
+        model = Sequential([
+            Input(shape=(28, 28, 1)),
+            Flatten(),
+            Dense(2500, activation="relu", input_dim=784),
+            # Para evitar el sobreajuste se eliminan nodos aleatoriamente
+            Dropout(0.2),
+            MaxPooling2D(pool_size=(2, 2)),
+            Dense(2000, activation="relu", input_dim=784),
+            # Para evitar el sobreajuste se eliminan nodos aleatoriamente
+            Dropout(0.2),
+            MaxPooling2D(pool_size=(2, 2)),
+            Dense(1500, activation="relu", input_dim=784),
+            # Para evitar el sobreajuste se eliminan nodos aleatoriamente
+            Dropout(0.2),
+            MaxPooling2D(pool_size=(2, 2)),
+            Dense(1000, activation="relu", input_dim=784),
+            # Para evitar el sobreajuste se eliminan nodos aleatoriamente
+            Dropout(0.2),
+            MaxPooling2D(pool_size=(2, 2)),
+            Dense(500, activation="relu", input_dim=784),
+            # Para evitar el sobreajuste se eliminan nodos aleatoriamente
+            Dropout(0.2),
+            MaxPooling2D(pool_size=(2, 2)),
+            Dense(10, activation="softmax")
+        ])
+    elif type == "rnn":
+        model = Sequential([
+            LSTM(2500, activation="relu", input_shape=784),
+            # Para evitar el sobreajuste se eliminan nodos aleatoriamente
+            Dropout(0.2),
+            MaxPooling2D(pool_size=(2, 2)),
+            LSTM(2000, activation="relu", input_shape=784),
+            # Para evitar el sobreajuste se eliminan nodos aleatoriamente
+            Dropout(0.2),
+            MaxPooling2D(pool_size=(2, 2)),
+            LSTM(1500, activation="relu", input_shape=784),
+            # Para evitar el sobreajuste se eliminan nodos aleatoriamente
+            Dropout(0.2),
+            MaxPooling2D(pool_size=(2, 2)),
+            LSTM(1000, activation="relu", input_shape=784),
+            # Para evitar el sobreajuste se eliminan nodos aleatoriamente
+            Dropout(0.2),
+            MaxPooling2D(pool_size=(2, 2)),
+            LSTM(500, activation="relu", input_shape=784),
+            # Para evitar el sobreajuste se eliminan nodos aleatoriamente
+            Dropout(0.2),
+            MaxPooling2D(pool_size=(2, 2)),
+            Dense(10, activation="softmax")
+        ])
+
+    x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+    y_train = to_categorical(y_train, 10)
+    x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+    y_test = to_categorical(y_test, 10)
+
+    #Normalizacion
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+
+    #sparse_categorical_crossentropy
+    model.compile(optimizer=SGD(learning_rate=0.05), loss="categorical_crossentropy", metrics=["accuracy"])
+    model.fit(x_train, y_train, epochs=30)
+    accuracy = model.evaluate(x_test, y_test)
+
+    print("Accuracy: " + str(round(accuracy[1], 3)))
+
+
 if __name__ == "__main__":
     epocas = 5
 
     perceptron = Perceptron()
-    train, train_labels, test, test_labels = perceptron.loadDataset()
+    x_train, y_train, x_test, y_test = perceptron.loadDataset()
 
     # showDigit(0)
-    w = perceptron.perceptron(train, train_labels, epocas=1, tipo="softmax", save_image=False, n=7)
-
-    for i in range(epocas):
-        y = perceptron.predict(w, train[i])
-        y_pred = perceptron.obtenerNumero(y)
-        print("Para i=" + str(i) + ", el nº es " + str(train_labels[i]) + " y el nº predicho es " + str(y_pred))
-
-    good = perceptron.test(test, test_labels)
-    print("Accurary: " + str(good) + "%")
+    #for i in range(epocas):
+    #    y = perceptron.predict(w, x_train[i])
+    #    y_pred = perceptron.obtenerNumero(y)
+    #    print("Para i=" + str(i) + ", el nº es " + str(y_train[i]) + " y el nº predicho es " + str(y_pred))
     #perceptron.plot()
+
+    #w = perceptron.perceptron(x_train, y_train, epocas=100, tipo="otro", save_image=False, n=7)
+    #good = perceptron.test(x_test, y_test)
+
+    #print("Accuracy Train: " + str(perceptron.accuracy[len(perceptron.accuracy)-1]))
+    #print("Accurary: " + str(good) + "%")
+
+    NN(x_train, y_train, x_test, y_test, "rnn")
